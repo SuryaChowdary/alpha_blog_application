12edf7953c87749b9763fae1af338b013af1412f
surya <surya@singular.dev>
2022-12-07 11:57:15 +0530

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username , presence: true ,
+                        uniqueness: {case_sensitive: false},  
+                        length: {minimum: 3 , maximum: 30}
+
+   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                    
+  validates :email , presence: true , 
+                    uniqueness: {case_sensitive: false},
+                    length: {maximum: 100},
+                    format: {with: EMAIL_REGEX}
 end
