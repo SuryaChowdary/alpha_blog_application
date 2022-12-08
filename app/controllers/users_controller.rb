1f38efc9c936c14bb5c2e3a2cb8206ec9f789798
surya <surya@singular.dev>
2022-12-08 15:40:50 +0530

    Added Controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new 
     @user = User.new
   end 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Surya-Blog #{@user.username} You've successfully signed up"
       redirect_to articles_path
     else
@@ -13,12 +17,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user =User.find(params[:id])
+
   end
 
   def update
 
-    @user= User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]= "User is updated successfully"
       redirect_to @user
@@ -28,7 +31,6 @@ class UsersController < ApplicationController
 
   end
   def show
-    @user =User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -42,6 +44,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username , :email, :password)
 
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
